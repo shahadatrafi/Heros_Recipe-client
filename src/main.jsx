b6ca7296c332755eb4556e4fb3fb250fdad40b08
ShahadatRafi <mdshahadathossenrafi@gmail.com>
2023-05-04 23:48:45 +0600

    Context Provider added

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
 import { RouterProvider } from 'react-router-dom';
 import router from './Components/Routes/Routes.jsx';
+import AuthProvider from './Components/Provider/AuthProvider.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <AuthProvider>
     <RouterProvider router={router} />
+    </AuthProvider>
   </React.StrictMode>,
 )
