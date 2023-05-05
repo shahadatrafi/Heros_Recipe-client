d6da8fee53363788daabc1bbdb6e8851a8b2e123
ShahadatRafi <mdshahadathossenrafi@gmail.com>
2023-05-05 12:42:07 +0600

    chef card added

--- src/Components/Pages/ChefCards/ChefCards.jsx
+++ src/Components/Pages/ChefCards/ChefCards.jsx
@@ -1,15 +1,38 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
-import { useLoaderData } from 'react-router-dom';
-
+import { Button, Card, Col, Container, Row } from 'react-bootstrap';
+import { Link, useLoaderData } from 'react-router-dom';
 const ChefCards = () => {
-    const allChefData = useLoaderData()
-    console.log(allChefData); 
+    const chefs = useLoaderData()
+    console.log(); 
     return (
         <Container>
-            <h4>Here is Our Chefs</h4>
+            <h4 className='text-center mt-5 mb-4'>Here is Our <span className='text-danger fw-bold'>Experienced</span> Chefs</h4>
+            <hr className='text-danger' />
             {
-                allChefData.map(cd=> {})
+                <Container>
+                <Row sm={1} md={3} className='g-3'>
+                    {chefs.map((chef) => (
+                        <Col key={chef.id} >
+                            <Card className='border border-danger'>
+                                <Card.Img variant="top" src={chef.chef_picture} />
+                                <Card.Body>
+                                    <Card.Title> Name: {chef.chef_name}</Card.Title>
+                                    <Card.Text className='fs-5'>
+                                        <span className='fw-semibold'>Experience:</span> {chef.years_of_experience}
+                                    </Card.Text>
+                                    <Card.Text>
+                                    <span className='fw-semibold'>Recipes:</span> {chef.num_recipes}
+                                    </Card.Text>
+                                    <Card.Text>
+                                    <span className='fw-semibold'>Likes:</span> {chef.likes}
+                                    </Card.Text>
+                                    <Link><Button variant="danger">View Recipes</Button></Link>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    ))}
+                </Row>
+            </Container>
             }
         </Container>
     );
