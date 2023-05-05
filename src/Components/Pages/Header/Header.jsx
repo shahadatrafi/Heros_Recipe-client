ac4549f515a12257fc4706dc625b694ee9d72d97
ShahadatRafi <mdshahadathossenrafi@gmail.com>
2023-05-05 22:31:54 +0600

    tootltip added

--- src/Components/Pages/Header/Header.jsx
+++ src/Components/Pages/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { Button, Image } from 'react-bootstrap';
+import { Button, Image, OverlayTrigger, Tooltip } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -27,6 +27,14 @@ const Header = () => {
       })
   }
 
+  const renderTooltip = () => {
+    return (
+      <Tooltip id="username-tooltip" style={{ color: 'red' }}>
+        {user.displayName}
+      </Tooltip>
+    );
+  };
+
   return (
     <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
       <Container>
@@ -38,9 +46,11 @@ const Header = () => {
             <Link className='fs-5 fw-bold me-3 text-secondary text-decoration-none' to='/blog'>Blog</Link>
           </Nav>
           <Nav>
-            {user?.uid ? <li><Image style={{ height: '50px' }} src={user?.photoURL} roundedCircle />
-              <Link><Button onClick={handleLogOut} className='fs-5 fw-bold ' variant="danger">Log Out</Button></Link></li> :
-              <Link to="/login"><Button className='fs-5 fw-bold ' variant="danger">Login</Button></Link>}
+            {user?.uid ? <li>
+              <Link> <OverlayTrigger placement="bottom" overlay={renderTooltip()}>
+              <Image className='me-2' style={{ height: '50px' }} src={user?.photoURL} roundedCircle />
+                                    </OverlayTrigger><Button onClick={handleLogOut} className='fs-5 fw-bold ' variant="danger">Log Out</Button></Link></li> :
+              <Link to="/login"><Button className='fs-5 fw-bold' variant="danger">Login</Button></Link>}
           </Nav>
         </Navbar.Collapse>
       </Container>
