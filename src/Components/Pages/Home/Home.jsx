cd5ab7570d293a455041a5556772e5388a07ed7a
Shahadat Rafi <mdshahadathossenrafi@gmail.com>
2023-08-26 00:24:49 +0600

    noting

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -8,10 +8,10 @@ import FeedbackSection from '../ClientFeedback/ClientFeedback ';
 const Home = () => {
     return (
         <Container>
-            <div className='d-flex align-items-center gap-5'>
+            <div className='d-flex align-items-center gap-5 '>
                 <div>
                     <h2 className='fs-1'>Heros <span className='text-danger'>Recipe</span></h2>
-                    <h4>Hero's Recipe is a food company that creates unique and healthy recipes using high-quality ingredients. </h4>
+                    <h4>Hero's Recipe is a recipe showcase app </h4>
                 </div>
                 <div>
                     <img src={banner} alt="" />
